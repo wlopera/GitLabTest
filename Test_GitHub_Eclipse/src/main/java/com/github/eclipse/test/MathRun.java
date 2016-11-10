3cd937e44fdd283cc03a951615b840833375d7bd
wlopera <lopera.william@gmail.com>
2016-11-10 13:53:25 -0500

    Implementar metodos de la interface

--- Test_GitHub_Eclipse/src/main/java/com/github/eclipse/test/MathRun.java
+++ Test_GitHub_Eclipse/src/main/java/com/github/eclipse/test/MathRun.java
@@ -1,17 +1,45 @@
 package com.github.eclipse.test;
 
 public class MathRun implements MathInterface {
-
+	private double width;
+	private double height;
+	
+	public MathRun(double width, double height){
+		this.width = width;
+		this.height = height;
+	}
+	
 	@Override
 	public String calculeAreaElement() {
-		// TODO Auto-generated method stub
-		return null;
+		double result =  width * height;
+		return "El area del cuadrado ["+width+","+height+"] es: " + result ;
 	}
 
 	@Override
 	public String getInformationElement() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "Rectangulo de ancho: " + width + " , alto: " + height;
 	}
 
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return "MathRun [width=" + width + ", height=" + height + "]";
+	}
+	
 }
