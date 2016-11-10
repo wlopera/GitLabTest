67b43cd473aaba85e92c2c2a66761ac03016d388
wlopera <lopera.william@gmail.com>
2016-11-10 13:58:14 -0500

    Se implemento el metodo main y se creo un objeto MathRun

--- Test_GitHub_Eclipse/src/main/java/com/github/eclipse/test/MathRun.java
+++ Test_GitHub_Eclipse/src/main/java/com/github/eclipse/test/MathRun.java
@@ -1,6 +1,13 @@
 package com.github.eclipse.test;
 
 public class MathRun implements MathInterface {
+	private double width;
+	private double height;
+	
+	public MathRun(double width, double height){
+		this.width = width;
+		this.height = height;
+	}
 
 	@Override
 	public String calculeAreaElement() {
@@ -14,4 +21,21 @@ public class MathRun implements MathInterface {
 		return null;
 	}
 
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+	
+	public static void main(String[] args){
+		MathRun mathRun = new MathRun(1500.0, 2500.0);
+		System.out.println("Se creo el objeto MathRun con 1500 y 25000 de ancho y alto");
+	}
+	
 }
