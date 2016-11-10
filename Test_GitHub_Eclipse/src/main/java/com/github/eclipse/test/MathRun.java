c9897fa2f2fb89b28b51581532a71430939756a9
wlopera <lopera.william@gmail.com>
2016-11-10 12:54:59 -0500

    Agregar MathRun

---
+++ Test_GitHub_Eclipse/src/main/java/com/github/eclipse/test/MathRun.java
@@ -0,0 +1,17 @@
+package com.github.eclipse.test;
+
+public class MathRun implements MathInterface {
+
+	@Override
+	public String calculeAreaElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getInformationElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
